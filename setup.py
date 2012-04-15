4580c96ccb54cb05ec0328847558706f1c17da09
Dan F-M <danfm@nyu.edu>
2012-04-15 12:02:49 -0400

    trying to fix setup script

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 from distutils.core import setup
 
-desc = """This basically does the same thing as
-python-casjobs (https://github.com/cosmonaut/python-casjobs) but it only
-depends on Requests (http://python-requests.org) which makes it more useful
-in my opinion."""
+desc = open("README.md").read()
+with open("requirements.txt") as f:
+    required = f.readlines()
 
 setup(
     name="casjobs",
     version="0.0.1",
     author="Daniel Foreman-Mackey",
     author_email="danfm@nyu.edu",
-    packages=["casjobs"],
     url="https://github.com/dfm/casjobs",
+    packages=["casjobs",],
+    install_requires=required,
     license="MIT",
     description="An interface to CasJobs for Humans.",
     long_description=desc,
