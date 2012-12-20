d7c88aa661c80540a4924e4a268fbde811da4ff2
Erik Tollerud <erik.tollerud@gmail.com>
2012-12-20 17:22:42 -0500

    Clarify "estimate" paramter in docstring
    I found the description currently in there rather confusing until I went and looked up the documentation on the SDSS site. This just clairfies what `estimate` is for (I'm also using it to try out the "edit" button on github, which I've never used for real before.)

--- casjobs.py
+++ casjobs.py
@@ -121,7 +121,7 @@ class CasJobs(object):
         ## Keyword Arguments
 
         * `task_name` (str): The task name.
-        * `estimate` (int): Estimate.
+        * `estimate` (int): Estimate if the time this job will take (in minutes).
 
         ## Returns
 
